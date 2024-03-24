e10cfd54a3a608eba2bcdd8f056cd7f3cbb0a962
Tech Lord <45688942+techlord001@users.noreply.github.com>
2024-03-24 15:26:49 +0000

    Add: Styling & Content For Verify View

--- client/src/views/Verify.vue
+++ client/src/views/Verify.vue
@@ -2,10 +2,14 @@
 </script>
 
 <template>
-  <div>
-    // TODO: Add verification message here
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <div class="p-8 border-2 border-border rounded-lg bg-secondary-background">
+      <h1 class="text-3xl font-bold mb-4 text-secondary-text">
+        A verification email has been sent to your email address.
+      </h1>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style>
 </style>
